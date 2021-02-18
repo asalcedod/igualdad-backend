57b9239ec0aafcf6d562df79be7a377aed67ecb9
Antonio D. Salcedo <antony9409@gmail.com>
2021-02-18 09:13:29 -0500

    update logger

--- config/logger.js
+++ config/logger.js
@@ -6,13 +6,13 @@ const logger = createLogger({
   transports: [
     new transports.MongoDB({
       level: "info",
-      db: process.env.MONGODB_URL,
+      db: "mongodb+srv://muto:OSrnIqun8SPlyqiv@clusteras.sqq9d.mongodb.net/igualdad?authSource=admin&replicaSet=atlas-vdd60w-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass%20Community&retryWrites=true&ssl=true",
       collection: "logs",
       format: format.combine(format.timestamp(), format.json()),
     }),
     new transports.MongoDB({
         level: "error",
-        db: process.env.MONGODB_URL,
+        db: "mongodb+srv://muto:OSrnIqun8SPlyqiv@clusteras.sqq9d.mongodb.net/igualdad?authSource=admin&replicaSet=atlas-vdd60w-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass%20Community&retryWrites=true&ssl=true",
         collection: "errors",
         format: format.combine(format.timestamp(), format.json()),
       }),
