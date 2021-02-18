18e5f403cf5a420fc4b19b91cbe8b21e558f8b78
Antonio D. Salcedo <antony9409@gmail.com>
2021-02-18 09:08:44 -0500

    db connection

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect("mongodb://127.0.0.1:27017/igualdad", { useNewUrlParser: true, seUnifiedTopology: true })
+    .connect("mongodb+srv://muto:OSrnIqun8SPlyqiv@clusteras.sqq9d.mongodb.net/igualdad?authSource=admin&replicaSet=atlas-vdd60w-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass%20Community&retryWrites=true&ssl=true", { useNewUrlParser: true, seUnifiedTopology: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
